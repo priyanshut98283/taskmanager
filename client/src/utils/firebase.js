70b536d10e428c5f50d17977a558574f17a834b5
Priyanshu Tiwari <priyanshut98283@gmail.com>
2024-09-08 23:41:37 +0530

    updated files

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBPdfBEzZXQvuh7cB2ThqkfBeJJmaIgtlI",
+    authDomain: "card-app-19.firebaseapp.com",
+    projectId: "card-app-19",
+    storageBucket: "card-app-19.appspot.com",
+    messagingSenderId: "210067052975",
+    appId: "1:210067052975:web:b6a5e5a4d4578d9347775a",
+    measurementId: "G-E9JVW0TNTY"
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
